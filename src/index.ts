71213f1e1e9a78bfa3cac1cf37d301c6d1088efb
Stiggu <abelmunda@hotmail.com>
2024-11-09 15:29:00 -0500

    Implementation of the CLI commands

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import { CardDefinition } from "./domain/cardDefinition.js";
+import { program } from "commander";
+import { buildCardDefinition } from "./domain/cardDefinition.js";
 import { CardEditor } from "./domain/cardEditor.js";
-import { Colors, Filters } from "./utils/utils.js";
 
-// Example card for now, will be replaced with CLI inputs!
-const definition: CardDefinition = {
-    color: Colors.OBJETDART,
-    filter: Filters.OBJETDART,
-    image: "./src/assets/ryoshu.jpg",
-    name: "S.T.A.B",
-    cost: 1,
-    attacks: [
-        {
-            name: "slash"
-        }
-    ]
-}
+program
+    .option('--cost <char>')
+    .option('--name <char>')
+    .option('--type <char>')
+    .option('--image <char>')
+
+program.parse()
+
+const definition = buildCardDefinition(program.opts())
 const cardEditor = new CardEditor(definition)
 
 cardEditor.load()
