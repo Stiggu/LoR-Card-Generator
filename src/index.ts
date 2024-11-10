25d966219103175f6a7e19f7c82978b5cf9d78ab
Stiggu <abelmunda@hotmail.com>
2024-11-09 20:29:52 -0500

    index refactor

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,6 @@ import { program } from "commander";
 import { buildCardDefinition } from "./domain/cardDefinition.js";
 import { CardEditor } from "./domain/cardEditor.js";
 
-program
-    .name('lor-card')
-    .description('CLI to generate a Card that looks like from Library of Ruina')
-    .version('0.0.1')
-    .option('-a, --attacks <char>')
-    .option('-c, --cost <char>')
-    .option('-i, --image <char>')
-    .option('-n, --name <char>')
-    .option('-r, --range <char>')
-    .option('-t, --type <char>')
-    .action(async (options) => {
-        await generate(options)
-    })
-
 const generate = async (option: Record<string, string|undefined>) => {
     const definition = buildCardDefinition(option)
     const cardEditor = new CardEditor(definition)
@@ -28,4 +14,15 @@ const generate = async (option: Record<string, string|undefined>) => {
     await cardEditor.card.save()
 }
 
-program.parse()
+program
+    .name('lor-card')
+    .description('CLI to generate a Card that looks like from Library of Ruina')
+    .version('0.0.1')
+    .option('-a, --attacks <char>')
+    .option('-c, --cost <char>')
+    .option('-i, --image <char>')
+    .option('-n, --name <char>')
+    .option('-r, --range <char>')
+    .option('-t, --type <char>')
+    .action(generate)
+    .parse()
